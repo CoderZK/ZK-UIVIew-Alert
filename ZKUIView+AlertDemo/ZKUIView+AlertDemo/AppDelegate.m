f4331b0491c430b81a061ed071cb38af4526dcab
zk542283618 <lanou@MAC-mini3.local>
2016-06-23 21:35:17 +0800

    update 初始化

--- ZKUIView+AlertDemo/ZKUIView+AlertDemo/AppDelegate.m
+++ ZKUIView+AlertDemo/ZKUIView+AlertDemo/AppDelegate.m
@@ -2,8 +2,8 @@
 //  AppDelegate.m
 //  ZKUIView+AlertDemo
 //
-//  Created by lanou on 16/6/23.
-//  Copyright © 2016年 lanou. All rights reserved.
+//  Created by ZK on 16/6/23.
+//  Copyright © 2016年 ZK. All rights reserved.
 //
 
 #import "AppDelegate.h"
--- ZKUIView+AlertDemo/ZKUIView+AlertDemo/ViewController.h
+++ ZKUIView+AlertDemo/ZKUIView+AlertDemo/ViewController.h
@@ -2,8 +2,8 @@
 //  ViewController.h
 //  ZKUIView+AlertDemo
 //
-//  Created by lanou on 16/6/23.
-//  Copyright © 2016年 lanou. All rights reserved.
+//  Created by ZK on 16/6/23.
+//  Copyright © 2016年 ZK. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
